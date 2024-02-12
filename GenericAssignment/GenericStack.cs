fb6766481937d01507f7b9d2ff7c52d53d899b88
navaneethsn <navaneethsn@LAP-2480>
2024-02-12 18:04:28 +0530

    Add project files.

---
+++ GenericAssignment/GenericStack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericAssignment
+{
+    internal class GenericStack<T>
+    {
+        private T[] items;
+        private int top;
+
+        public GenericStack(int size)
+        {
+            items = new T[size];
+            top = -1;
+        }
+
+        public void Push(T item = default(T))
+        {
+            if (!typeof(T).IsValueType)
+            {
+                throw new InvalidOperationException("Cannot insert no value types");
+            }
+            if (top == items.Length - 1)
+            {
+                Console.WriteLine("Stack is full");
+                return;
+            }
+            top++;
+            items[top] = item;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Stack is Empty");
+            }
+            T item = items[top];
+            top--;
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            T item = items[top];
+            return item;
+        }
+
+        public bool IsEmpty()
+        {
+            return top == -1;
+        }
+
+    }
+}
---
+++ GenericAssignment/Program.cs
@@ -0,0 +1,102 @@
+﻿//using Generic;
+using GenericAssignment;
+
+class Program
+{
+    static void Main(string[] args) { 
+    
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("What is your stack type ( 1.INT 2.DOUBLE 3.CHAR )");
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    HandleStack<int>();
+                    break;
+                case "2":
+                    HandleStack<double>();
+                    break;
+                case "3":
+                    HandleStack<char>();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
+
+    }
+
+    static void HandleStack<T>()
+    {
+        GenericStack<T> stack = new GenericStack<T>(10);
+        bool running = true;
+        while (running)
+        {
+            Console.WriteLine("\nChoose Operation: ");
+            Console.WriteLine("1.Push");
+            Console.WriteLine("2.Pop");
+            Console.WriteLine("3.Peek");
+            Console.WriteLine("4.IsEmpty");
+            Console.WriteLine("5.Exit");
+            running = StackOperations<T>(stack);
+        }
+    }
+
+    static bool StackOperations<T>(GenericStack<T> stack)
+    {
+        bool running = true;
+        string choice = Console.ReadLine();
+        switch (choice)
+        {
+            case "1":
+                Console.WriteLine($"Enter {typeof(T)}element to push");
+                string element = Console.ReadLine();
+                try
+                {
+                    if (element != "")
+                        stack.Push((T)Convert.ChangeType(element, typeof(T)));
+                    else
+                        stack.Push();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Invalid input");
+                }
+
+                break;
+            case "2":
+                if (stack.IsEmpty())
+                {
+                    Console.WriteLine("Stack is Empty");
+                }
+                else
+                {
+                    Console.WriteLine("Popped Item : " + stack.Pop());
+                }
+                break;
+            case "3":
+                if (stack.IsEmpty())
+                {
+                    Console.WriteLine("Stack is Empty");
+                }
+                else
+                {
+                    Console.WriteLine("Peek: " + stack.Peek());
+                }
+                break;
+            case "4":
+                Console.WriteLine("Is stack empty ? : " + stack.IsEmpty());
+                break;
+            case "5":
+                running = false;
+                break;
+            default:
+                Console.WriteLine("Invalid choice");
+                break;
+        }
+        return running;
+    }
+}
